6d57b6e098dfc6d814f5e6e773c49ea9911a7b4a
pooja5-creator <py5825590@gmail.com>
2025-03-13 15:39:42 +0530

    Add Redirect File

--- src/components/ShowLoadData.jsx
+++ src/components/ShowLoadData.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 export default function ShowLoadData() {
   return (
-    <div className='w-full min-h-[300px]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
-    lg:grid-cols-4 gap-3 
-    place-items-center'>
+    <div className='w-full min-h-[300px]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 place-items-center'>
       {Array.from({ length: 4 }).map((el, i) => (
         <div key={i} className="w-[100%] h-[350px] bg-gray-200 rounded-xl p-3 animate-pulse">
           <div className="w-full h-[150px] bg-gray-400 rounded-lg mb-4"></div>
